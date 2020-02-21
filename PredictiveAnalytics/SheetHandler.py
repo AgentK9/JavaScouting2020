991bdd75396d6a20ce5fe10e9b22fc56d5246e4b
Keegan Birt <kbirt@birtdata.com>
2020-02-20 19:07:37 -0500

    Finish data get from sheet hopefully

--- PredictiveAnalytics/SheetHandler.py
+++ PredictiveAnalytics/SheetHandler.py
@@ -2,6 +2,10 @@ from PredictiveAnalytics.SheetsClient import *
 from PredictiveAnalytics.PredAnaLyb import *
 
 
+def bool_from_yes_no(yesno: str):
+    return True if "yes" in yesno.lower() else False if "no" in yesno.lower() else None
+
+
 def get_team_data(service):
     raw = get_data(service=service, location="JavaScouting 2020 - Match Observation!B2:W100")
     teams = {}
@@ -11,11 +15,31 @@ def get_team_data(service):
             "alliance_color": row[2].lower(),
         }
         if "total" in row[3].lower():
-            x = 0  # TODO: total based data translation
+            match["score"] = int(row[4])
         elif "phase" in row[3].lower():
-            x = 0  # TODO: phase based data translation
+            match["auto_score"] = int(row[5])
+            match["tele-op_score"] = int(row[6])
+            match["end_game_score"] = int(row[7])
         elif "team" in row[3].lower():
-            x = 0  # TODO: team based data translation
+            match["autonomous"] = {
+                "num_skystones_delivered": int(row[8]),
+                "num_stones_delivered": int(row[9]),
+                "num_stones_placed": int(row[10]),
+                "moved_foundation": bool_from_yes_no(row[11]),
+                "parked": bool_from_yes_no(row[12])
+            }
+            match["tele-op"] = {
+                "num_stones_delivered": int(row[13]),
+                "num_stones_placed": int(row[14]),
+                "tallest_skyscraper_levels": int(row[15])
+            }
+            match["end_game"] = {
+                "capped": bool_from_yes_no(row[16]),
+                "num_cap_levels": int(row[17]),
+                "moved_foundation": bool_from_yes_no(row[18]),
+                "parked": bool_from_yes_no(row[19])
+            }
+
 
         if str(row[0]) not in teams.keys():
             teams[str(row[0])] = {}
